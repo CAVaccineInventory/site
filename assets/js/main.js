bb7234a9ba21fcfdab51b1fadea12eb589d05bb2
Jackson Mohsenin <jmohsenin@gmail.com>
2021-01-17 09:55:49 -0800

    menu bar navigation on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 // Stub for custom JS
+
+window.onload = () => {
+  document.querySelector(".nav-button").addEventListener("click", (e) => {
+    document.querySelector("nav").classList.toggle("open");
+    e.preventDefault();
+  });
+};
