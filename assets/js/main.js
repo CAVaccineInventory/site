be1672c51de6b3059d090ff2151d97a8d4ca9783
Jackson Mohsenin <jmohsenin@gmail.com>
2021-01-17 16:29:20 -0800

    Add county search field (#22)
    * Add county search field
    
    * counties list: add alameda
    
    * County select: Clear filter on blank enter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,38 @@
 "use strict";
 
+import counties from "./counties.js";
+
 window.onload = () => {
   document.querySelector(".nav-button").addEventListener("click", (e) => {
     document.querySelector("nav").classList.toggle("is_open");
     e.preventDefault();
   });
+
+  const input = document.querySelector("#autoComplete");
+  if (input) {
+    const airtable = document.querySelector(".airtable-embed");
+    const airtableURL = airtable.src;
+
+    new autoComplete({
+      data: {
+        src: counties,
+      },
+      selector: "#autoComplete",
+      maxResults: 7,
+      highlight: true,
+      onSelection: (feedback) => {
+        const selected = feedback.selection.value;
+
+        input.value = selected;
+        airtable.src = airtableURL + "&filter_County=" + selected;
+      },
+    });
+
+    // If a user clears the search field and hits enter, reset to unfiltered table
+    input.addEventListener("keydown", (e) => {
+      if (e.key == "Enter" && input.value.length == 0) {
+        airtable.src = airtableURL;
+      }
+    });
+  }
 };
