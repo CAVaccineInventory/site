75ae4f4864494a3399777bfd27231441027a4fc9
eliblock <3347571+eliblock@users.noreply.github.com>
2021-01-18 23:27:19 -0800

    chore: hide test locations from reports

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,11 +1,17 @@
 // Utilities for working with the JSON feed
 function getHasVaccine(p) {
-  return p["Latest report yes?"] == 1 && p["Location Type"] != "Test Location";
+  try {
+    return p["Latest report yes?"] == 1 && p["Location Type"] != "Test Location";
+  } catch {
+    return false
+  }
 }
 
 function getHasReport(p) {
   try {
-    return p["Has Report"];
+    if (p["Location Type"] != "Test Location") {
+      return p["Has Report"];
+    }
   } catch {
     return false;
   }
