cc9fcd646a585c46536bd8678e56a5b863afd5a9
Mike Skalnik <mike@skalnik.com>
2021-02-05 23:46:14 -0800

    Setup purging 🔥

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      "./**/*.html",
+      "./**/*.md",
+      "./**/*.yml",
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
