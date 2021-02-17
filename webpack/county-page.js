41b036a0b00b8c92e1d3ea8964da2ad6e5a73de1
Mike Skalnik <mike@skalnik.com>
2021-02-16 16:32:11 -0800

    Add county policy cards to county pages 📃

--- webpack/county-page.js
+++ webpack/county-page.js
@@ -6,8 +6,13 @@ import {
 } from "./data/locations.js";
 
 import { addSitesToPage } from "./sites.js";
+import { fetchCounties } from "./data/counties.js";
+import policyTemplate from "./templates/policy.handlebars";
+import marked from "marked";
+import sanitizeHtml from "sanitize-html";
 
 window.addEventListener("load", fetchCountySites);
+window.addEventListener("load", fetchCountyCard);
 
 function addSitesOrRemoveIfEmpty(sites, containerId) {
   if (!sites.length) {
@@ -18,9 +23,13 @@ function addSitesOrRemoveIfEmpty(sites, containerId) {
   }
 }
 
+function currentCounty() {
+  return document.getElementById("county_name").textContent.trim();
+}
+
 async function fetchCountySites() {
   let sites = await fetchSites();
-  const county = document.getElementById("county_name").textContent.trim();
+  const county = currentCounty();
 
   // Filter by this page's county.
   sites = sites.filter((site) => {
@@ -38,4 +47,25 @@ async function fetchCountySites() {
   addSitesOrRemoveIfEmpty(sitesWithVaccine, "sitesWithVaccine");
   addSitesOrRemoveIfEmpty(sitesWithoutVaccine, "sitesWithoutVaccine");
   addSitesOrRemoveIfEmpty(sitesWithNoReport, "sitesWithoutReport");
+}
+
+async function fetchCountyCard() {
+  const countyPolicies = await fetchCounties();
+  const countyPolicy = countyPolicies.find((county) => county["County"] === currentCounty());
+
+  let notes = countyPolicy["Notes"];
+  notes = sanitizeHtml(marked(notes));
+
+  const templateInfo = {
+    name: countyPolicy["County"],
+    infoURL: countyPolicy["Vaccine info URL"],
+    locationsURL: countyPolicy["Vaccine locations URL"],
+    volunteering: countyPolicy["Official volunteering opportunities"],
+    reservationURL: countyPolicy["countyPolicy vaccination reservations URL"],
+    facebook: countyPolicy["Facebook Page"],
+    twitter: countyPolicy["Twitter Page"],
+    notes: notes,
+  };
+
+  document.querySelector(".js-county-policy").innerHTML = policyTemplate(templateInfo);
 }
