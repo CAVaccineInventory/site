68a409006a08d989cecffaa7fe6850f7611822ed
Mike Skalnik <mike@skalnik.com>
2021-02-25 16:01:29 -0800

    Markdownify the notes 📑

--- webpack/providers.js
+++ webpack/providers.js
@@ -1,5 +1,6 @@
 import { fetchProviders } from "./data/providers.js";
 import providerTemplate from "./templates/provider.handlebars";
+import { markdownify } from "./markdown.js";
 
 window.addEventListener("load", loadProviders);
 
@@ -8,13 +9,18 @@ async function loadProviders() {
   const providerList = document.getElementById("js-providers");
 
   for (const provider of providers) {
+    let notes = provider["Public Notes"];
+    if (notes) {
+      notes = markdownify(notes);
+    }
+
     const templateInfo = {
       name: provider["Provider"],
       type: provider["Provider network type"],
       infoURL: provider["Vaccine info URL"],
       locationURL: provider["Vaccine locations URL"],
       appointmentURL: provider["Appointments URL"],
-      notes: provider["Public Notes"],
+      notes: notes,
     };
 
     providerList.innerHTML += providerTemplate(templateInfo);
