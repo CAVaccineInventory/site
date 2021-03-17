3240923ca5bbae15aefec3a680c13eb4538bdac3
Mike Skalnik <mike@skalnik.com>
2021-03-17 15:45:58 -0700

    Build custom marked to open links in new tab 📑

--- webpack/markdown.js
+++ webpack/markdown.js
@@ -1,12 +1,43 @@
 import marked from "marked";
 import sanitizeHtml from "sanitize-html";
 
+const newTabLinkRenderer = {
+  link(href, title, text) {
+    // Logic adapted from https://github.com/markedjs/marked/issues/655#issuecomment-712380889
+    const localLink = href.startsWith(`${location.protocol}//${location.hostname}`);
+    const html = linkRenderer.call(renderer, href, title, text);
+    if (localLink) {
+      return html;
+    }
+
+    return html.replace(/^<a /, "<a target='_blank' rel='noreferrer noopener nofollow' ");
+  },
+};
+
+function customMarked() {
+  const renderer = new marked.Renderer();
+  const linkRenderer = renderer.link;
+  renderer.link = (href, title, text) => {
+    const html = linkRenderer.call(renderer, href, title, text);
+
+    if (href.startsWith(`${location.protocol}//${location.hostname}`)) {
+      return html;
+    }
+
+    return html.replace(/^<a /, '<a target="_blank" rel="nofollow" ');
+  };
+
+  marked.use({ renderer });
+
+  return marked;
+}
+
 function markdownify(str) {
-  return sanitizeHtml(marked(str));
+  return sanitizeHtml(customMarked()(str, newTabLinkRenderer));
 }
 
 function markdownifyInline(str) {
-  return sanitizeHtml(marked.parseInline(str));
+  return sanitizeHtml(customMarked().parseInline(str));
 }
 
 export { markdownify, markdownifyInline };
