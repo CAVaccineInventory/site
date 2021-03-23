bb77374cd2f362abd114ccef1e86a075519de12c
Mike Skalnik <mike@skalnik.com>
2021-03-22 20:07:15 -0700

    We don't need this anymore 🔥

--- webpack/markdown.js
+++ webpack/markdown.js
@@ -1,24 +1,6 @@
 import marked from "marked";
 import sanitizeHtml from "sanitize-html";
 
-const newTabLinkRenderer = {
-  link(href, title, text) {
-    // Logic adapted from https://github.com/markedjs/marked/issues/655#issuecomment-712380889
-    const localLink = href.startsWith(
-      `${location.protocol}//${location.hostname}`
-    );
-    const html = linkRenderer.call(renderer, href, title, text);
-    if (localLink) {
-      return html;
-    }
-
-    return html.replace(
-      /^<a /,
-      "<a target='_blank' rel='noreferrer noopener nofollow' "
-    );
-  },
-};
-
 function customMarked() {
   const renderer = new marked.Renderer();
   const linkRenderer = renderer.link;
