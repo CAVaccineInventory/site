d064bd5a2070bbfdb1e1000a62b82f732c523bf5
Alec Lee <alec.lee15@gmail.com>
2021-04-20 16:20:49 -0700

    give embed components a white background, remove copy button from embed

--- webpack/sites.js
+++ webpack/sites.js
@@ -42,6 +42,9 @@ function addSitesToPage(sites, containerId) {
       notes = markdownify(notes);
     }
 
+    // only render copy button if site has a vaccine because otherwise when the link is used the site
+    // will be hidden by default. Also no copy button for embeds because of iframe permissions.
+    const withCopyButton = info.hasVaccine == "yes" && window.location.pathname !== "/embed";
     const context = {
       id: info.id,
       name: info.name,
@@ -65,6 +68,7 @@ function addSitesToPage(sites, containerId) {
       notes: notes,
       providerInfo: info.providerNotes,
       id: info.id,
+      withCopyButton
     };
 
     const range = document
